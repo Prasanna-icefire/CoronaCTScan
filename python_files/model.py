ffde788ceb01a6a001f0061ce3b4752b4350d2d9
Prasanna-icefire <prasannamg3@gmail.com>
2020-05-04 17:16:49 +0530

    Checking best fit model

--- python_files/model.py
+++ python_files/model.py
@@ -6,8 +6,9 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout,MaxPooling2D, Activation
 from tensorflow.keras.callbacks import TensorBoard
 import time
-NAME = "cats_vs_Dogs_FINALFINALCNN_64*2-{}".format(int(time.time()))
-tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+
+
+
 
 X = pickle.load(open("X.pickle","rb"))
 y = pickle.load(open("y.pickle","rb"))
@@ -15,22 +16,35 @@ y = pickle.load(open("y.pickle","rb"))
 #For gray scale images 0 represents black wile 255 is white.....So we need to normalize all the pixel values, convert every pixel in the range 0 to 1
 X=X/255.0
 
-model = Sequential()
-model.add(Conv2D(64,(3,3),input_shape=X.shape[1:]))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(64,(3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Conv2D(64,(3,3)))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(Dense(64))
-model.add(Activation('relu'))
-model.add(Dense(1))
-model.add(Activation('sigmoid'))
-model.compile(loss="binary_crossentropy",optimizer="adam",metrics=['accuracy'])
-model.fit(np.array(X),np.array(y), batch_size=32,epochs = 6, validation_split=0.1,callbacks=[tensorboard])
 
-#model.save('corona.model')
+
+dense_layers = [0,1,2,3,4]
+layer_sizes = [32,64,128,256]
+conv_layers = [1,2,3,4]
+
+for dense_layer in dense_layers:
+    for layer_size in layer_sizes:
+        for conv_layer in conv_layers:
+            NAME ="{}-conv-{}-nodes-{}-dense".format(conv_layer,layer_size,dense_layer,int(time.time()))
+            tensorboard = TensorBoard(log_dir='logs/{}'.format(NAME))
+            print(NAME)
+            model = Sequential()
+            model.add(Conv2D(layer_size,(3,3),input_shape=X.shape[1:]))
+            model.add(Activation("relu"))
+            model.add(MaxPooling2D(pool_size=(2,2)))
+
+            for l in range(conv_layer-1):
+                model.add(Conv2D(layer_size,(3,3)))
+                model.add(Activation("relu"))
+                model.add(MaxPooling2D(pool_size=(2,2)))
+            model.add(Flatten())    
+            for l in range(dense_layer):
+                model.add(Dense(layer_size))
+                model.add(Activation('relu'))
+
+            model.add(Dense(1))
+            model.add(Activation('sigmoid'))
+            model.compile(loss="binary_crossentropy",optimizer="adam",metrics=['accuracy'])
+            model.fit(np.array(X),np.array(y), batch_size=13,epochs = 10, validation_split=0.1,callbacks=[tensorboard])
+
+            #model.save('corona.model')
