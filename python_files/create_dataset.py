b508ed1a0b8a56831b38e0d71d0762dcf345a969
Prasanna-icefire <prasannamg3@gmail.com>
2020-05-04 14:17:00 +0530

    Created a data_set

--- python_files/create_dataset.py
+++ python_files/create_dataset.py
@@ -20,5 +20,22 @@ def create_training_data():
                 training_data.append([newArray,class_num])
             except Exception as e:
                 pass
-create_training_data()            
-print(len(training_data))
+create_training_data()
+random.shuffle(training_data)            
+
+X = []
+y = []
+
+for features,labels in training_data:
+    X.append(features)
+    y.append(labels)
+
+X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+pickle_out = open("X.pickle","wb")
+pickle.dump(X,pickle_out)
+pickle_out.close()
+pickle_out = open("y.pickle","wb")
+pickle.dump(y,pickle_out)
+pickle_out.close()
+
+
